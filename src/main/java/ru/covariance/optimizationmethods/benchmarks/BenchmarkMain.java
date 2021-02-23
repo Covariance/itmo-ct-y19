b8991a1000953d2f0ed1866886ee5b7ec985dde3
CovarianceMomentum <covariancemomentum@gmail.com>
2021-02-23 18:17:27 +0300

    Added JMH for benchmarks

---
+++ src/main/java/ru/covariance/optimizationmethods/benchmarks/BenchmarkMain.java
@@ -0,0 +1,11 @@
+package ru.covariance.optimizationmethods.benchmarks;
+
+import java.io.IOException;
+
+public class BenchmarkMain {
+
+  public static void main(String[] args) throws IOException {
+    org.openjdk.jmh.Main.main(args);
+  }
+}
+
