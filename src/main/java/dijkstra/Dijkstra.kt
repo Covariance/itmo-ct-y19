4c0e91a3a1ab7c082c16b06aa50f80aaac70aca8
Pavel Martynov <covariancemomentum@gmail.com>
2022-01-19 21:46:35 +0300

    Task solved

--- src/main/java/dijkstra/Dijkstra.kt
+++ src/main/java/dijkstra/Dijkstra.kt
@@ -2,10 +2,79 @@ package dijkstra
 
 import java.util.*
 import java.util.concurrent.Phaser
-import kotlin.Comparator
+import java.util.concurrent.ThreadLocalRandom
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.locks.ReentrantLock
 import kotlin.concurrent.thread
 
-private val NODE_DISTANCE_COMPARATOR = Comparator<Node> { o1, o2 -> Integer.compare(o1!!.distance, o2!!.distance) }
+private val NODE_DISTANCE_COMPARATOR = Comparator<Node> { o1, o2 -> o1!!.distance.compareTo(o2!!.distance) }
+
+class MultiQueue<E>(qsc: Int, private val cmp: Comparator<E>) {
+    private val qs: Array<PriorityQueue<E>> = Array(qsc) { PriorityQueue(cmp) }
+    private val locks: Array<ReentrantLock> = Array(qsc) { ReentrantLock() }
+
+    private fun lock(): Pair<Int, Int> {
+        var a = ThreadLocalRandom.current().nextInt(0, qs.size)
+        var b = ThreadLocalRandom.current().nextInt(0, qs.size)
+
+        while (b == a) {
+            b = ThreadLocalRandom.current().nextInt(0, qs.size)
+        }
+
+        if (a > b) {
+            a = b.also { b = a }
+        }
+
+        locks[a].lock()
+        locks[b].lock()
+
+        return Pair(a, b)
+    }
+
+    private fun unlock(which: Pair<Int, Int>) {
+        locks[which.second].unlock()
+        locks[which.first].unlock()
+    }
+
+    fun add(el: E) {
+        val (a, b) = lock()
+
+        if (qs[a].size < qs[b].size) {
+            qs[a].add(el)
+        } else {
+            qs[b].add(el)
+        }
+
+        unlock(Pair(a, b))
+    }
+
+    fun poll(): E? {
+        val (a, b) = lock()
+
+        val topA = qs[a].peek()
+        val topB = qs[b].peek()
+        if (topA == null && topB == null) {
+            unlock(Pair(a, b))
+            return null
+        }
+        if (topA == null) {
+            qs[b].poll()
+            unlock(Pair(a, b))
+            return topB
+        }
+        if (topB == null) {
+            qs[a].poll()
+            unlock(Pair(a, b))
+            return topA
+        }
+
+        val result = (if (cmp.compare(topA, topB) < 0) qs[a] else qs[b]).poll()
+
+        unlock(Pair(a, b))
+
+        return result
+    }
+}
 
 // Returns `Integer.MAX_VALUE` if a path has not been found.
 fun shortestPathParallel(start: Node) {
@@ -13,26 +82,31 @@ fun shortestPathParallel(start: Node) {
     // The distance to the start node is `0`
     start.distance = 0
     // Create a priority (by distance) queue and add the start node into it
-    val q = PriorityQueue(workers, NODE_DISTANCE_COMPARATOR) // TODO replace me with a multi-queue based PQ!
+    val q = MultiQueue(4, NODE_DISTANCE_COMPARATOR)
     q.add(start)
     // Run worker threads and wait until the total work is done
     val onFinish = Phaser(workers + 1) // `arrive()` should be invoked at the end by each worker
+    val activeNodes = AtomicInteger(1)
     repeat(workers) {
         thread {
-            while (true) {
-                // TODO Write the required algorithm here,
-                // TODO break from this loop when there is no more node to process.
-                // TODO Be careful, "empty queue" != "all nodes are processed".
-//                val cur: Node? = synchronized(q) { q.poll() }
-//                if (cur == null) {
-//                    if (workIsDone) break else continue
-//                }
-//                for (e in cur.outgoingEdges) {
-//                    if (e.to.distance > cur.distance + e.weight) {
-//                        e.to.distance = cur.distance + e.weight
-//                        q.addOrDecreaseKey(e.to)
-//                    }
-//                }
+            while (activeNodes.get() > 0) {
+                val cur = q.poll() ?: continue
+                for (e in cur.outgoingEdges) {
+                    // println("edge from " + cur.toString() + " to " + e.to.toString() + " with weight " + e.weight.toString())
+                    while (true) {
+                        val dst = e.to.distance
+                        if (dst > cur.distance + e.weight) {
+                            if (e.to.casDistance(dst, cur.distance + e.weight)) {
+                                q.add(e.to)
+                                activeNodes.incrementAndGet()
+                                break
+                            }
+                        } else {
+                            break
+                        }
+                    }
+                }
+                activeNodes.decrementAndGet()
             }
             onFinish.arrive()
         }
