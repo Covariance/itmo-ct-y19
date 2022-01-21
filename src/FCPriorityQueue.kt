92bb98037a4fd6cf27a32b4f181ff1b91cc06c95
Pavel Martynov <covariancemomentum@gmail.com>
2022-01-21 23:56:06 +0300

    This solution is so damn beautiful

--- src/FCPriorityQueue.kt
+++ src/FCPriorityQueue.kt
@@ -1,7 +1,74 @@
-import java.util.*
+import kotlinx.atomicfu.atomic
+import kotlinx.atomicfu.atomicArrayOfNulls
+import java.util.PriorityQueue
+import java.util.concurrent.ThreadLocalRandom
+
+private const val FC_SIZE = 16
 
 class FCPriorityQueue<E : Comparable<E>> {
+    private class Completed(val result: Any?)
+
     private val q = PriorityQueue<E>()
+    private val lock = atomic(false)
+    private val actions = atomicArrayOfNulls<Any?>(FC_SIZE)
+
+    private fun tryLock(): Boolean {
+        return lock.compareAndSet(expect = false, update = true)
+    }
+
+    private fun unlock() {
+        lock.compareAndSet(expect = true, update = false)
+    }
+
+    private fun combine() {
+        for (ind in 0 until FC_SIZE) {
+            val status = actions[ind].value
+
+            if (status !is Completed && status != null) { // It's an actual operation there!
+                @Suppress("UNCHECKED_CAST")
+                actions[ind].compareAndSet(status, (status as () -> Completed)())
+            }
+        }
+    }
+
+    private fun <T> act(action: () -> T?): T? {
+        // First we try to execute our action
+        if (tryLock()) {
+            // If we got the lock, then we become the combiner
+            val result = action()
+            combine()
+            return result.also { unlock() }
+        }
+
+        // Otherwise, we publish our action to random cell
+        var ind = ThreadLocalRandom.current().nextInt(FC_SIZE)
+
+        val wrappedAction: () -> Completed = { Completed(action()) }
+
+        while (!actions[ind].compareAndSet(null, wrappedAction)) {
+            // If cell is taken, move to the next one
+            ind = (ind + 1) % FC_SIZE
+        }
+
+        // And wait for result
+        while (true) {
+            val status = actions[ind].value
+
+            // Our task is completed by combiner, we can just take it
+            if (status is Completed) {
+                val result = status.result
+                actions[ind].compareAndSet(status, null)
+                @Suppress("UNCHECKED_CAST")
+                return result as T?
+            }
+
+            // Also try to become combiner
+            // Do not worry about result, we would take it on next iteration
+            if (tryLock()) {
+                combine().also { unlock() }
+            }
+        }
+    }
 
     /**
      * Retrieves the element with the highest priority
@@ -9,7 +76,7 @@ class FCPriorityQueue<E : Comparable<E>> {
      * returns `null` if the queue is empty.
      */
     fun poll(): E? {
-        return q.poll()
+        return act { q.poll() }
     }
 
     /**
@@ -17,13 +84,13 @@ class FCPriorityQueue<E : Comparable<E>> {
      * or `null` if the queue is empty.
      */
     fun peek(): E? {
-        return q.peek()
+        return act { q.peek() }
     }
 
     /**
      * Adds the specified element to the queue.
      */
     fun add(element: E) {
-        q.add(element)
+        act { q.add(element) }
     }
 }
