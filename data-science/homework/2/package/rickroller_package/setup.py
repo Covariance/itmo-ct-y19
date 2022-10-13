25dce310ae43d7f10ddaa555488d48f4fb8737ae
Pavel Martynov <covariancemomentum@gmail.com>
2022-10-13 22:42:28 +0300

    dependabot vulnerability fix

--- data-science/homework/2/package/rickroller_package/setup.py
+++ data-science/homework/2/package/rickroller_package/setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='rickroller_package',
-    version='0.1.0',    
+    version='0.1.0',
     description='An example Python package for andan lab',
     url='https://www.youtube.com/watch?v=dQw4w9WgXcQ',
     author='Pavel Martynov',
     author_email='covariancemomentum@gmail.com',
     license='MIT License',
     packages=['rickroller_package'],
-    install_requires=['numpy<=1.17.*']
+    install_requires=['numpy>=1.22.*']
 )
